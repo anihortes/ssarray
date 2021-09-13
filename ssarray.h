d8980bdd2ef66654e43c7bac5efa87855ec1b7a8
Adam Owen <ahowenjr@alaska.edu>
2021-09-12 23:07:54 -0800

    ugh

--- ssarray.h
+++ ssarray.h
@@ -7,11 +7,12 @@
 
 #include <iostream>
 #include <string> // for std::string
-#include <utility> // for std::move. std::swap
+#include <utility> // for std::move, std::swap
 #include <ostream> // for std::ostream
 #include <cassert> // for std::assert
 #include <cstddef> // for std::size_t
-#include <algorithm> // for std::swap, std::equal
+#include <algorithm> // for std::swap, std::equal, std::fill
+
 template <typename valType>
 class SSArray
 {
@@ -21,37 +22,56 @@ public:
 
     //default ctor
     SSArray(): _arrayPtr(new value_type[8]), _arraySize(8){
+/*
+        std::fill(iter begin, iter begin+7, 0);
 
+
+        size_type index = 0;
+        while(true){
+            _arrayPtr[index] = 0;
+            if(index == 8) break;
+            index++;
+        }*/
     }
 
     //ctor
     explicit SSArray(size_type size)
         : _arrayPtr(new value_type[size]), _arraySize(size) {
+/*
+            std::fill(iter begin, iter begin+size-1, 0);
 
+           size_type index = 0;
+        while(true){
+            _arrayPtr[index] = index;
+            if(index == size) break;
+            index++;
+        }*/
     }
 
     //ctor
     SSArray(size_type size, value_type value)
         : _arrayPtr(new value_type[size]), _arraySize(size){
-        /*
-         for(size_type i = 0; i<_arraySize;i++){
-            _arrayPtr[i] = value;
-        }
-        */
-    }
+        //std::fill(_arrayPtr, _arrayPtr+size-1, 0);
 
-/*
-    SSArray(SSArray & other):_arrayPtr(other._arrayPtr), _arraySize(other._arraySize){
-        for(int i = 0; i<_arraySize;i++){
-            _arrayPtr[i] = other[i];
+        size_type index = 0;
+        while(true){
+            assert(index < size);
+            _arrayPtr[index] = value;
+            if(index == size) break;
+            index++;
         }
+
     }
-    */
 
     //copy ctor
-    SSArray(const SSArray & other): _arrayPtr(new value_type[other.size()]), _arraySize(other._arraySize){
-        for(int i = 0; i<_arraySize;i++){
-            _arrayPtr[i] = other[i];
+    SSArray(const SSArray & other): _arrayPtr(new value_type[other._arraySize]), _arraySize(other._arraySize){
+       // std::copy(other.begin(),other.end(),_arrayPtr??)
+        size_type index = 0;
+        while(true){
+            assert(index < other.size());
+            _arrayPtr[index] = other[index];
+            if(index == other.size()) break;
+            index++;
         }
     }
 
@@ -106,7 +126,7 @@ public:
     }
 
     value_type * end(){
-        return begin() + _arraySize + 1;
+        return begin() + _arraySize;
     }
 
     const value_type * begin() const{
@@ -114,7 +134,7 @@ public:
     }
 
     const value_type * end() const{
-        return begin() + _arraySize + 1;
+        return begin() + _arraySize;
     }
 
 private:
@@ -124,7 +144,7 @@ private:
     }
 
     value_type * _arrayPtr;
-    int _arraySize{};
+    size_type _arraySize;
 };
 
 //Preconditions:
@@ -132,59 +152,62 @@ private:
 
 template <class SSArray>
 bool operator==(const SSArray & rhs, const SSArray & lhs){
-    assert(std::equal(rhs.begin(),rhs.end(),lhs.begin()));
+    //assert(std::equal(rhs.begin(),rhs.end(),lhs.begin()));
     if(rhs.size() == lhs.size()){
-        int equalCompare = 0;
+        if(std::equal(rhs.begin(),rhs.end(),lhs.begin(),lhs.end()))
+            return true;
+        /*int equalCompare = 0;
         for(int i = 0; i < rhs.size(); i++){
             if (rhs.begin()+i == lhs.begin()+i) {
                 equalCompare++;
             }
         }
         if(equalCompare==rhs.size())
-            return true;
+            return true;*/
     }
     return false;
 }
 
 template <class SSArray>
 bool operator!=(const SSArray & rhs, const SSArray & lhs){
-    assert(std::equal(rhs.begin(),rhs.end(),lhs.begin()));
+    //assert(std::equal(rhs.begin(),rhs.end(),lhs.begin()));
     return !(rhs==lhs);
 }
 
 template <class SSArray>
 bool operator<(const SSArray & rhs, const SSArray & lhs){
-    std::size_t counter=0;
-    while(rhs.begin()+counter != rhs.end() && lhs.begin()+counter != lhs.end()){
-        if(rhs[counter]<lhs[counter])
-            return true;
-        else if(rhs[counter]>lhs[counter])
-            return false;
-        else counter++;
+    //assert(std::equal(rhs.begin(),rhs.end(),lhs.begin()));
+    auto mypair = std::mismatch(rhs.begin(),rhs.end(),lhs);
+    while(mypair.first != rhs.end() && mypair.second != lhs.end()) {
+        if (mypair.first < mypair.second) return true;
+        mypair.first++;
+        mypair.second++;
     }
     return false;
 }
 
 template <class SSArray>
 bool operator<=(const SSArray & rhs, const SSArray & lhs){
-    std::size_t counter=0;
-    while(rhs.begin()+counter != rhs.end() && lhs.begin()+counter != lhs.end()){
-        if(rhs[counter]<lhs[counter])
-            return true;
-        else if(rhs[counter]>lhs[counter])
-            return false;
-        else counter++;
+    //assert(std::equal(rhs.begin(),rhs.end(),lhs.begin()));
+    auto mypair = std::mismatch(rhs.begin(),rhs.end(),lhs);
+    while(mypair.first != rhs.end() && mypair.second!=lhs.end()) {
+        if (mypair.first < mypair.second) return true;
+        if (mypair.first > mypair.second) return false;
+        mypair.first++;
+        mypair.second++;
     }
     return true;
 }
 
 template <class SSArray>
 bool operator>(const SSArray & rhs, const SSArray & lhs){
+    //assert(std::equal(rhs.begin(),rhs.end(),lhs.begin()));
     return !(rhs <= lhs);
 }
 
 template <class SSArray>
 bool operator>=(const SSArray & rhs, const SSArray & lhs){
+    //assert(std::equal(rhs.begin(),rhs.end(),lhs.begin()));
     return !(rhs < lhs);
 }
 #endif //SSARRAY_SSARRAY_H
