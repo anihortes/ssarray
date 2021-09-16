e907cea591661a00a2ec0a9042309ae60e9932d9
Adam Owen <ahowenjr@alaska.edu>
2021-09-15 23:36:00 -0800

    improved comments

--- ssarray.h
+++ ssarray.h
@@ -1,37 +1,35 @@
-//
-// Created by A. Harrison Owen on 9/10/2021.
-//
+// ssarray.h
+// A. Harrison Owen
+// Started: 9/10/2021
+// Finished: 9/16/2021
+// Semi-Smart Array Class
+//      template smart array class,
+//      can do basic boolean compare operations
 
 #ifndef SSARRAY_SSARRAY_H
 #define SSARRAY_SSARRAY_H
 
-#include <iostream>
-#include <string> // for std::string
-#include <utility> // for std::move, std::swap
-#include <ostream> // for std::ostream
-#include <cassert> // for std::assert
-#include <cstddef> // for std::size_t
-#include <algorithm> // for std::swap, std::equal, std::fill
+#include <cassert>   // for std::assert
+#include <cstddef>   // for std::size_t
+#include <algorithm> // for std::swap
 
 template <typename valType>
-class SSArray
-{
+class SSArray {
 public:
-    using value_type = valType;
-    using size_type = std::size_t;
+    // generic types for template
+    using value_type = valType;    // value type in array
+    using size_type = std::size_t; // size type in array
 
-    //default ctor
+    //default ctor, size = 8, value = whatever is in memory
     SSArray(): _arrayPtr(new value_type[8]), _arraySize(8){}
 
-    //ctor
+    //ctor with specified size
     explicit SSArray(size_type size)
         : _arrayPtr(new value_type[size]), _arraySize(size) {}
 
-    //ctor
+    //ctor with specified size and value
     SSArray(size_type size, value_type value)
         : _arrayPtr(new value_type[size]), _arraySize(size){
-        //std::fill(_arrayPtr, _arrayPtr+size-1, 0);
-
         for(std::size_t i = 0; i < _arraySize; i++){
             _arrayPtr[i] = value;
         }
@@ -67,10 +65,12 @@ public:
         other._arraySize=0;
     }
 
+    // dctor
     ~SSArray(){
         delete [] _arrayPtr;
     }
 
+//[] operators : array[n]
 public:
 
     value_type & operator[](size_type index){
@@ -83,13 +83,17 @@ public:
         return _arrayPtr[index];
     }
 
+// iterator functions
 public:
 
     size_type size(){
+        assert(end()-begin() >= 0);
         return (end()-begin());
     }
 
+
     [[nodiscard]] size_type size() const{
+        assert(end()-begin() >= 0);
         return (end() - begin());
     }
 
@@ -109,11 +113,15 @@ public:
         return begin() + _arraySize;
     }
 
+// private data members and functions
 private:
+    // for copy and move assignment operators
     void arraySwap(SSArray & other) noexcept{
         std::swap(_arrayPtr, other._arrayPtr);
         std::swap(_arraySize, other._arraySize);
     }
+
+    // generic types for template
     value_type *_arrayPtr;
     size_type _arraySize;
 };
@@ -121,12 +129,14 @@ private:
 //Preconditions:
 //Requirements on Types
 
+// lhs == rhs is true : lhs[n] == rhs[n]
+//                      AND lhs.size() == rhs.size()
 template <class value_type>
 bool operator==(const SSArray<value_type> & lhs, const SSArray<value_type> & rhs){
     auto rhsIter = rhs.begin();
     auto lhsIter = lhs.begin();
     // if sizes arent equal, then rhs != lhs
-    if(rhs.size()!=lhs.size()) return false;
+    if(rhs.size() != lhs.size()) return false;
     while (rhsIter != rhs.end() && lhsIter != lhs.end()) {
         if (*rhsIter != *lhsIter) return false;
         rhsIter++;
@@ -140,33 +150,37 @@ bool operator!=(const SSArray & lhs, const SSArray & rhs){
     return !(lhs==rhs);
 }
 
+// lhs < rhs is true : lhs[0] < rhs[0]
+//                     if lhs[0] == rhs[0] then check for lhs[n+1] < rhs[n+1] ...
+//                     if lhs[n+1] == rhs[n+1] for all n then lhs.size() < rhs.size() returns true
 template <class value_type>
 bool operator<(const SSArray<value_type> & lhs, const SSArray<value_type> & rhs){
+    
     // count will be the range of the for-loop
     // compare rhs and lhs sizes and choose smallest one
     std::size_t count=0;
-    if(rhs.size()>lhs.size())count=lhs.size();
+    if(rhs.size() > lhs.size())count=lhs.size();
     else count = rhs.size();
 
-    for(std::size_t i=0; i<count; i++){
-        if(lhs[i]<rhs[i]) return true;
-        else if(lhs[i]>rhs[i])return false;
+    for(std::size_t i = 0; i < count; i++){
+        if(lhs[i] < rhs[i]) return true;
+        else if(lhs[i] > rhs[i]) return false;
     }
-    return(lhs.size() < rhs.size());
+    return (lhs.size() < rhs.size());
 }
 
 template <class SSArray>
 bool operator<=(const SSArray & lhs, const SSArray & rhs){
-    return!(rhs<lhs);
+    return!(rhs < lhs);
 }
 
 template <class SSArray>
 bool operator>(const SSArray & lhs, const SSArray & rhs){
-    return(rhs<lhs);
+    return(rhs < lhs);
 }
 
 template <class SSArray>
 bool operator>=(const SSArray & lhs, const SSArray & rhs){
-    return !(lhs<rhs);
+    return !(lhs < rhs);
 }
 #endif //SSARRAY_SSARRAY_H
